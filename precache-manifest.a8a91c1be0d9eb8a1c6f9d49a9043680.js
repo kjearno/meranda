5ae290cbb1e72f08c263ae6e31067c522604f076
kjearno <kaa.96t@gmail.com>
2022-11-12 01:56:51 +0500

    Updates

---
+++ precache-manifest.a8a91c1be0d9eb8a1c6f9d49a9043680.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9f021b8a030df92f63a3e8331f34ea",
+    "url": "/meranda/index.html"
+  },
+  {
+    "revision": "e0fedd4bc5f53245ed9a",
+    "url": "/meranda/static/css/2.6dba0ebc.chunk.css"
+  },
+  {
+    "revision": "0e3cd90b75f1bd078135",
+    "url": "/meranda/static/css/main.51f682bd.chunk.css"
+  },
+  {
+    "revision": "e0fedd4bc5f53245ed9a",
+    "url": "/meranda/static/js/2.0cae22b5.chunk.js"
+  },
+  {
+    "revision": "a979189b752ee6d79ab214bdfa0c14ea",
+    "url": "/meranda/static/js/2.0cae22b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3cd90b75f1bd078135",
+    "url": "/meranda/static/js/main.5174c7c0.chunk.js"
+  },
+  {
+    "revision": "d365461a7272a8fb3e86",
+    "url": "/meranda/static/js/runtime-main.68d20f75.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/meranda/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/meranda/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/meranda/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/meranda/static/media/slick.f97e3bbf.svg"
+  }
+]);
