f0a348435098bbfc80b21460700061769c694df2
kjearno <kaa.96t@gmail.com>
2022-12-10 13:36:37 +0500

    Remove external ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["@features", "./src/features"],
+          ["@shared", "./src/shared"],
+        ],
+        extensions: [".ts", ".js", ".jsx", ".json"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
